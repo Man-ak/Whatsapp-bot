68c535e1eeae18c2dbd6ac5b87e5f43939dc9463
Manak Jain <kotharimanak9421@gmail.com>
2023-09-29 15:20:44 +0530

    Add files via upload

---
+++ Whatsapp bot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pywhatkit
+from datetime import datetime
+
+PHONE_NUMBER = input("Enter Phone Number: ")
+MESSAGE = input("Enter Message: ")
+HR = int(input("Enter Hour: "))     # 24 Hour Format
+MIN = int(input("Enter Minutes:"))
+
+pywhatkit.sendwhatmsg(PHONE_NUMBER, MESSAGE, HR, MIN)
+
